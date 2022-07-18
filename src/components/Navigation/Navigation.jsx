7605bcf886d2159bbe99c29aa71664c9da93903a
Egidijus Bizas <egidijus.bizas@gmail.com>
2022-07-18 18:45:12 +0300

    add logo to navbar and favicon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../assets/logo72.png'
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg" className='sticky-top shadow p-3 mb-5 bg-white rounded'>
       <Container>
+      <a class="navbar-brand" href="#">
+      <img src={logo} alt="logo" height="36"></img>
+      </a>
         <Navbar.Brand href="#home">Scroll Away</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
