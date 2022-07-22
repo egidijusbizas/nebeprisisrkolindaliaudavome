e390d9f60412d3b4bb9a3b4b51ae75f77d9e86fe
Egidijus Bizas <egidijus.bizas@gmail.com>
2022-07-22 14:58:44 +0300

    fix layout

--- src/pages/PictureDetails.js
+++ src/pages/PictureDetails.js
@@ -10,24 +10,26 @@ function PictureDetails() {
   const sizes = Object.keys(urls);
 
   return (
-    <div className='container filler'>
-      <div className='column'>
-        <button type='button' className='btn btn-light btn-lg container' onClick={() => navigate(-1)}>
-          Back
-        </button>
+    <div className='container'>
+      <div className='container filler'>
+        <div className='column'>
+          <button type='button' className='btn btn-light btn-lg fullwidth' onClick={() => navigate(-1)}>
+            Back
+          </button>
 
-        <div className='gallery__item_box'>
-          <h1>Image {id}</h1>
-          <img src={urls.thumb}></img>
-          <p>Get alternate sizes</p>
-          <div className='list-group'>
-            {sizes.map((name, idx) => {
-              return (
-                <a href={urls[name]} key={idx} className='list-group-item list-group-item-action'>
-                  {name}
-                </a>
-              );
-            })}
+          <div className='gallery__item_box'>
+            <h1>Image {id}</h1>
+            <img src={urls.thumb}></img>
+            <p>Get alternate sizes</p>
+            <div className='list-group'>
+              {sizes.map((name, idx) => {
+                return (
+                  <a href={urls[name]} key={idx} className='list-group-item list-group-item-action'>
+                    {name}
+                  </a>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
